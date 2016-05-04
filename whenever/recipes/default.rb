d62cd61beb0daa9d925731da60712cb1e33788e1
LightSky <mikeh+github@lightsky.com>
2016-05-04 09:37:57 -0500

    Update default.rb
    Removed roles declaration from command

--- whenever/recipes/default.rb
+++ whenever/recipes/default.rb
@@ -15,7 +15,8 @@ node[:deploy].each do |application, deploy|
 
     cwd "#{deploy[:deploy_to]}/current"
     user 'deploy'
-    code "bundle exec whenever --set environment=#{deploy[:rails_env]} --update-crontab #{application} --roles #{layers.join(',')}"
+    #code "bundle exec whenever --set environment=#{deploy[:rails_env]} --update-crontab #{application} --roles #{layers.join(',')}"
+    code "bundle exec whenever --set environment=#{deploy[:rails_env]} --update-crontab #{application}"
     only_if "cd #{deploy[:deploy_to]}/current && bundle show whenever"
   end
 end
